0a11aa82089f82c6c464dea12db6aabff5a69b5e
Ivan Prodeus <learning.webpractice@gmail.com>
2022-08-23 23:02:44 +0300

    completed task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+const refs = {
+    body: document.querySelector('body'),
+    btnStart: document.querySelector("button[data-start]"),
+    btnStop: document.querySelector("button[data-stop]"),
+};
 
+refs.body.addEventListener('click', colorChanger);
+
+function colorChanger(event){
+  
+  if (event.target === refs.btnStart){
+    refs.btnStart.setAttribute ('disabled', true);
+    return timerId = setInterval(() => {
+      refs.body.setAttribute('style', `background-color: ${getRandomHexColor()}`);
+      }, 1000);  
+  }else if (event.target === refs.btnStop){
+    refs.btnStart.removeAttribute ('disabled');
+    refs.btnStop.setAttribute ('disabled', true);
+    clearInterval(timerId);
+  }
+};
