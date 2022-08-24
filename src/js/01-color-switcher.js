c12f53de8910d50df7a27f20bb9bfef44f1657f7
Ivan Prodeus <learning.webpractice@gmail.com>
2022-08-24 22:29:36 +0300

    fixed task 1 and added css on button

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,30 +1,26 @@
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
 const refs = {
-    body: document.querySelector('body'),
-    btnStart: document.querySelector("button[data-start]"),
-    btnStop: document.querySelector("button[data-stop]"),
+  body: document.querySelector('body'),
+  btnStart: document.querySelector("button[data-start]"),
+  btnStop: document.querySelector("button[data-stop]"),
 };
 
-refs.btnStart.addEventListener('click', colorChanger);
-refs.btnStop.addEventListener('click', stopColorChange);
-function bodyColors(){
-  refs.body.setAttribute('style', `background-color: ${getRandomHexColor()}`);
-};
-
+refs.body.addEventListener('click', colorChanger);
+let timerId = null;
 function colorChanger(event) {
-  refs.btnStart.setAttribute ('disabled', true);
-  const timerId = setInterval(() => {
-    bodyColors();
-    }, 1000); 
-};
 
-function stopColorChange(event){
-    refs.btnStart.removeAttribute ('disabled');
-    refs.btnStop.setAttribute ('disabled', true);
+  if (event.target === refs.btnStart) {
+    refs.btnStart.setAttribute('disabled', true);
+    timerId = setInterval(() => {
+      refs.body.setAttribute('style', `background-color: ${getRandomHexColor()}`);
+    }, 1000);
+  } else if (event.target === refs.btnStop) {
+    refs.btnStart.removeAttribute('disabled');
+    refs.btnStop.setAttribute('disabled', true);
     clearInterval(timerId);
-    console.log('stop',timerId);
-  };
+  }
+};
   
 //ТРЕБА ПЕРЕОБІТИ НЕ ВІРНО ВИКОНУЄТЬСЯ
