42d4ba46b886eb3d957243ebc2d29f1169e161e5
Ivan Prodeus <learning.webpractice@gmail.com>
2022-08-31 20:37:47 +0300

    task3 in progress

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import Notiflix, { Notify } from "notiflix";
+import { refs } from "./referece_for_promise_elements";
+
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  
+  return new Promise((resolve, reject) =>{
+    const shouldResolve = Math.random() > 0.3;
+    const step = Number(refs.inputStep.value);
+    const number = setTimeout(() => {
+      if (shouldResolve) {
+        resolve(console.log('good', position)); //(Notify.success('teste GOOD', position));
+      } else {
+        reject(console.log('bad', position)); //(Notify.failure('test BAD', position));
+      };
+    }, delay);
+delay += step
+    console.log(delay, step);
+    console.log(number);
+  });
+};
+
+
+
+function promisGeneration(event) {
+  event.preventDefault();
+
+  const amount = Number(refs.inputAmount.value);
+  let delay = Number(refs.inputDelay.value);
+  
+
+  for(let i = 1; i<= amount; i++){
+    createPromise(i, delay)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
   }
-}
+};
+
+refs.submitButton.addEventListener ('click', promisGeneration) 
+  
---
+++ src/js/referece_for_promise_elements.js
@@ -0,0 +1,16 @@
+
+
+export const refs = {
+    inputDelay: document.querySelector('[name="delay"]'),
+  
+    inputStep: document.querySelector('[name="step"]'),
+  
+    inputAmount: document.querySelector('[name="amount"]'),
+
+    submitButton: document.querySelector('button')
+
+};
+
+// console.log(refs.inputDelay);
+// console.log(refs.inputStep);
+// console.log(refs.inputAmount);
