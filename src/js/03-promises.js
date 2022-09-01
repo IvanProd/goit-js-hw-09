77151345b4e4782e4b073f280e6a722021b2375b
Ivan Prodeus <learning.webpractice@gmail.com>
2022-09-01 19:23:23 +0300

    finished task3 (not exciting but very intresting)

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,25 +2,20 @@ import Notiflix, { Notify } from "notiflix";
 import { refs } from "./referece_for_promise_elements";
 
 
-
 function createPromise(position, delay) {
   
   return new Promise((resolve, reject) =>{
     const shouldResolve = Math.random() > 0.3;
-    const step = Number(refs.inputStep.value);
-    const number = setTimeout(() => {
+    setTimeout(() => {
       if (shouldResolve) {
-        resolve(console.log('good', position)); //(Notify.success('teste GOOD', position));
-      } else {
-        reject(console.log('bad', position)); //(Notify.failure('test BAD', position));
-      };
+        resolve({position, delay});
+      } 
+        reject({position, delay}); 
+      
     }, delay);
-delay += step
-    console.log(delay, step);
-    console.log(number);
+
   });
 };
-
 
 
 function promisGeneration(event) {
@@ -28,18 +23,19 @@ function promisGeneration(event) {
 
   const amount = Number(refs.inputAmount.value);
   let delay = Number(refs.inputDelay.value);
-  
+  const step = Number(refs.inputStep.value);
 
   for(let i = 1; i<= amount; i++){
     createPromise(i, delay)
   .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    Notify.success(`Fulfilled promise ${position} in ${delay} ms`);
   })
   .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    Notify.failure(`Rejected promise ${position} in ${delay} ms`);
   });
-  }
+  delay += step
+  };
+  
 };
 
-refs.submitButton.addEventListener ('click', promisGeneration) 
-  
+refs.submitButton.addEventListener ('click', promisGeneration);
